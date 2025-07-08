de0917c47c2a33eb024cd11b2fe1a05a7a32090a
Dudu Matias <engedumatias@gmail.com>
2025-07-07 21:57:02 -0300

    Melhorando a lógica usando o swich case

--- src/Desafio.java
+++ src/Desafio.java
@@ -27,25 +27,33 @@ public class Desafio {
             System.out.println(menu);
             opcao = leitura.nextInt();
 
-            if (opcao == 1){
-                System.out.println("O saldo atualizado é " + saldo);
-            } else if (opcao == 2) {
-                System.out.println("Qual o valor que deseja transferir?");
-                double valor = leitura.nextDouble();
-                if (valor > saldo) {
-                    System.out.println("Não há saldo para realizar a transferência.");
-                } else {
-                    saldo -= valor;
+            switch (opcao) {
+                case 1:
+                    System.out.println("O saldo atualizado é " + saldo);
+                    break;
+                case 2:
+                    System.out.println("Qual o valor que deseja transferir?");
+                    double valor = leitura.nextDouble();
+                    if (valor > saldo) {
+                        System.out.println("Não há saldo para realizar a transferência.");
+                    } else {
+                        saldo -= valor;
+                        System.out.println("Novo saldo: " + saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Valor recebido: ");
+                    double valorRecebido = leitura.nextDouble();
+                    saldo += valorRecebido;
                     System.out.println("Novo saldo: " + saldo);
-                }
-            } else if (opcao == 3) {
-                System.out.println("Valor recebido: ");
-                double valor = leitura.nextDouble();
-                saldo += valor;
-                System.out.println("Novo saldo: " + saldo);
-            } else if (opcao != 4) {
-                System.out.println("Opção inválida!");
+                    break;
+                case 4:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
             }
         }
     }
 }
+
